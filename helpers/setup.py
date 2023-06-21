2ddf67be19fc068d4f4f9def3845eea2ed1c3472
Orest Tokovenko <oresttokovenko@gmail.com>
2023-06-20 23:26:22 -0600

    Change snowflake setup file to write env to `.env`. Add `orders` and `customers`

--- helpers/setup.py
+++ helpers/setup.py
@@ -2,7 +2,7 @@ snowflake_user = input("Enter Snowflake username: ")
 snowflake_pass = input("Enter Snowflake password: ")
 snowflake_account_id = input("Enter Snowflake account_id: (ex. E9VQG1LF-HBGF7017) ")
 
-with open(".env_test", "w") as f:
+with open(".env", "w") as f:
     f.write(f"SNOWFLAKE_ACCOUNT_ID={snowflake_account_id}\n")
     f.write(f"SNOWFLAKE_PASSWORD={snowflake_pass}\n")
     f.write(f"SNOWFLAKE_USERNAME={snowflake_user}\n")
