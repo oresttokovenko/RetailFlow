8ee750e6a9a573d108bde0b0882687cb66454fd7
Orest Tokovenko <oresttokovenko@gmail.com>
2023-07-11 11:16:20 -0600

    [helpers] Refactor setup_airbyte_environment and setup_credentials scripts

--- helpers/setup_airbyte_environment.py
+++ helpers/setup_airbyte_environment.py
@@ -4,175 +4,111 @@ import os
 
 SQLALCHEMY_SILENCE_UBER_WARNING=1
 
-# getting connection string for snowflake instance
-load_dotenv()
-user = os.getenv("SNOWFLAKE_USERNAME")
-password = os.getenv("SNOWFLAKE_PASSWORD")
-account_identifier = os.getenv("SNOWFLAKE_ACCOUNT_ID")
+def get_engine():
+    # getting connection string for snowflake instance
+    load_dotenv()
+    user = os.getenv("SNOWFLAKE_USERNAME")
+    password = os.getenv("SNOWFLAKE_PASSWORD")
+    account_identifier = os.getenv("SNOWFLAKE_ACCOUNT_ID")
 
-engine = create_engine(f"snowflake://{user}:{password}@{account_identifier}/")
+    return create_engine(f"snowflake://{user}:{password}@{account_identifier}/")
 
+def execute_queries(engine):
+    # if you plan to use explicit transactions, you must disable the AUTOCOMMIT execution option
+    with engine.connect().execution_options(autocommit=False) as connection:
+        try:
+            # set variables (these need to be uppercase)
+            connection.execute("SET airbyte_role = 'AIRBYTE_ROLE'")
+            connection.execute("SET airbyte_username = 'AIRBYTE_USER'")
+            connection.execute("SET airbyte_warehouse = 'AIRBYTE_WAREHOUSE'")
+            connection.execute("SET airbyte_database = 'AIRBYTE_DATABASE'")
+            connection.execute("SET airbyte_schema = 'AIRBYTE_SCHEMA'")
 
-# if you plan to use explicit transactions, you must disable the AUTOCOMMIT execution option
-with engine.connect().execution_options(autocommit=False) as connection:
-    try:
-        # set variables (these need to be uppercase)
-        connection.execute("SET airbyte_role = 'AIRBYTE_ROLE'")
-        connection.execute("SET airbyte_username = 'AIRBYTE_USER'")
-        connection.execute("SET airbyte_warehouse = 'AIRBYTE_WAREHOUSE'")
-        connection.execute("SET airbyte_database = 'AIRBYTE_DATABASE'")
-        connection.execute("SET airbyte_schema = 'AIRBYTE_SCHEMA'")
+            # set user password
+            connection.execute("SET airbyte_password = 'password'")
 
-        # set user password
-        connection.execute("SET airbyte_password = 'password'")
+            connection.execute("BEGIN")
 
-        connection.execute("BEGIN")
+            # create Airbyte role
+            connection.execute("USE ROLE securityadmin")
+            connection.execute("CREATE ROLE IF NOT EXISTS identifier($airbyte_role)")
+            connection.execute("GRANT ROLE identifier($airbyte_role) TO ROLE SYSADMIN")
 
-        # create Airbyte role
-        connection.execute("USE ROLE securityadmin")
-        connection.execute("CREATE ROLE IF NOT EXISTS identifier($airbyte_role)")
-        connection.execute("GRANT ROLE identifier($airbyte_role) TO ROLE SYSADMIN")
-
-        # create Airbyte user
-        connection.execute(
+            # create Airbyte user
+            connection.execute(
+                """
+                CREATE USER IF NOT EXISTS identifier($airbyte_username)
+                PASSWORD = $airbyte_password
+                DEFAULT_ROLE = $airbyte_role
+                DEFAULT_WAREHOUSE = $airbyte_warehouse
             """
-            CREATE USER IF NOT EXISTS identifier($airbyte_username)
-            PASSWORD = $airbyte_password
-            DEFAULT_ROLE = $airbyte_role
-            DEFAULT_WAREHOUSE = $airbyte_warehouse
-        """
-        )
+            )
 
-        connection.execute(
-            "GRANT ROLE identifier($airbyte_role) TO USER identifier($airbyte_username)"
-        )
+            connection.execute(
+                "GRANT ROLE identifier($airbyte_role) TO USER identifier($airbyte_username)"
+            )
 
-        # change role to sysadmin for warehouse / database steps
-        connection.execute("USE ROLE sysadmin")
+            # change role to sysadmin for warehouse / database steps
+            connection.execute("USE ROLE sysadmin")
 
-        # create Airbyte warehouse
-        connection.execute(
+            # create Airbyte warehouse
+            connection.execute(
+                """
+                CREATE WAREHOUSE IF NOT EXISTS identifier($airbyte_warehouse)
+                WAREHOUSE_SIZE = 'XSMALL'
+                WAREHOUSE_TYPE = 'STANDARD'
+                AUTO_SUSPEND = 60
+                AUTO_RESUME = TRUE
+                INITIALLY_SUSPENDED = TRUE
             """
-            CREATE WAREHOUSE IF NOT EXISTS identifier($airbyte_warehouse)
-            WAREHOUSE_SIZE = 'XSMALL'
-            WAREHOUSE_TYPE = 'STANDARD'
-            AUTO_SUSPEND = 60
-            AUTO_RESUME = TRUE
-            INITIALLY_SUSPENDED = TRUE
-        """
-        )
+            )
 
-        # create Airbyte database
-        connection.execute(
-            "CREATE DATABASE IF NOT EXISTS identifier($airbyte_database)"
-        )
+            # create Airbyte database
+            connection.execute(
+                "CREATE DATABASE IF NOT EXISTS identifier($airbyte_database)"
+            )
 
-        # grant Airbyte warehouse access
-        connection.execute(
-            "GRANT USAGE ON WAREHOUSE identifier($airbyte_warehouse) TO ROLE identifier($airbyte_role)"
-        )
+            # grant Airbyte warehouse access
+            connection.execute(
+                "GRANT USAGE ON WAREHOUSE identifier($airbyte_warehouse) TO ROLE identifier($airbyte_role)"
+            )
 
-        # grant Airbyte database access
-        connection.execute(
-            "GRANT OWNERSHIP ON DATABASE identifier($airbyte_database) TO ROLE identifier($airbyte_role)"
-        )
+            # grant Airbyte database access
+            connection.execute(
+                "GRANT OWNERSHIP ON DATABASE identifier($airbyte_database) TO ROLE identifier($airbyte_role)"
+            )
 
-        connection.execute("COMMIT")
+            connection.execute("COMMIT")
 
-        connection.execute("BEGIN")
+            connection.execute("BEGIN")
 
-        # use the created database
-        connection.execute("USE DATABASE identifier($airbyte_database)")
+            # use the created database
+            connection.execute("USE DATABASE identifier($airbyte_database)")
 
-        # create schema for Airbyte data
-        connection.execute("CREATE SCHEMA IF NOT EXISTS identifier($airbyte_schema)")
+            # create schema for Airbyte data
+            connection.execute("CREATE SCHEMA IF NOT EXISTS identifier($airbyte_schema)")
 
-        connection.execute("COMMIT")
+            connection.execute("COMMIT")
 
-        connection.execute("BEGIN")
+            connection.execute("BEGIN")
 
-        # grant Airbyte schema access
-        connection.execute(
-            "GRANT OWNERSHIP ON SCHEMA identifier($airbyte_schema) TO ROLE identifier($airbyte_role)"
-        )
+            # grant Airbyte schema access
+            connection.execute(
+                "GRANT OWNERSHIP ON SCHEMA identifier($airbyte_schema) TO ROLE identifier($airbyte_role)"
+            )
 
-        connection.execute("COMMIT")
-        print("all queries executed")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        connection.execute("ROLLBACK")
-    finally:
-        connection.close()
+            connection.execute("COMMIT")
+            print("all queries executed")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            connection.execute("ROLLBACK")
+        finally:
+            connection.close()
 
-engine.dispose()
+def main():
+    engine = get_engine()
+    execute_queries(engine)
+    engine.dispose()
 
-### airbyte_environment_setup.sql ###
-
-# -- set variables (these need to be uppercase)
-# set airbyte_role = 'AIRBYTE_ROLE';
-# set airbyte_username = 'AIRBYTE_USER';
-# set airbyte_warehouse = 'AIRBYTE_WAREHOUSE';
-# set airbyte_database = 'AIRBYTE_DATABASE';
-# set airbyte_schema = 'AIRBYTE_SCHEMA';
-
-# -- set user password
-# set airbyte_password = 'password';
-
-# begin;
-
-# -- create Airbyte role
-# use role securityadmin;
-# create role if not exists identifier($airbyte_role);
-# grant role identifier($airbyte_role) to role SYSADMIN;
-
-# -- create Airbyte user
-# create user if not exists identifier($airbyte_username)
-# password = $airbyte_password
-# default_role = $airbyte_role
-# default_warehouse = $airbyte_warehouse;
-
-# grant role identifier($airbyte_role) to user identifier($airbyte_username);
-
-# -- change role to sysadmin for warehouse / database steps
-# use role sysadmin;
-
-# -- create Airbyte warehouse
-# create warehouse if not exists identifier($airbyte_warehouse)
-# warehouse_size = xsmall
-# warehouse_type = standard
-# auto_suspend = 60
-# auto_resume = true
-# initially_suspended = true;
-
-# -- create Airbyte database
-# create database if not exists identifier($airbyte_database);
-
-# -- grant Airbyte warehouse access
-# grant USAGE
-# on warehouse identifier($airbyte_warehouse)
-# to role identifier($airbyte_role);
-
-# -- grant Airbyte database access
-# grant OWNERSHIP
-# on database identifier($airbyte_database)
-# to role identifier($airbyte_role);
-
-# commit;
-
-# begin;
-
-# USE DATABASE identifier($airbyte_database);
-
-# -- create schema for Airbyte data
-# CREATE SCHEMA IF NOT EXISTS identifier($airbyte_schema);
-
-# commit;
-
-# begin;
-
-# -- grant Airbyte schema access
-# grant OWNERSHIP
-# on schema identifier($airbyte_schema)
-# to role identifier($airbyte_role);
-
-# commit;
+if __name__ == "__main__":
+    main()
--- helpers/setup_credentials.py
+++ helpers/setup_credentials.py
@@ -1,18 +1,24 @@
-open('.env_lambda', 'a').close()
+def get_user_input():
+    snowflake_user = input("Enter Snowflake username: ")
+    snowflake_pass = input("Enter Snowflake password: ")
+    snowflake_account_id = input("Enter Snowflake account_id: (ex. E9VQG1LF-HBGF7017) ")
+    aws_account_id = input("Enter AWS account ID: (ex. 012345678901) ")
+    return snowflake_user, snowflake_pass, snowflake_account_id, aws_account_id
 
-snowflake_user = input("Enter Snowflake username: ")
-snowflake_pass = input("Enter Snowflake password: ")
-snowflake_account_id = input("Enter Snowflake account_id: (ex. E9VQG1LF-HBGF7017) ")
-aws_account_id = input("Enter AWS account ID: (ex. 012345678901) ")
+def write_to_file(filename, snowflake_user, snowflake_pass, snowflake_account_id, aws_account_id):
+    with open(filename, "w") as f:
+        f.write(f"SNOWFLAKE_ACCOUNT_ID={snowflake_account_id}\n")
+        f.write(f"SNOWFLAKE_PASSWORD={snowflake_pass}\n")
+        f.write(f"SNOWFLAKE_USERNAME={snowflake_user}\n")
+        if "AWS_ACCOUNT_ID" in filename:
+            f.write(f"AWS_ACCOUNT_ID={aws_account_id}\n")
+        if "SNOWFLAKE_ROLE" in filename:
+            f.write(f"SNOWFLAKE_ROLE={snowflake_user}\n")
 
-with open(".env", "w") as f:
-    f.write(f"SNOWFLAKE_ACCOUNT_ID={snowflake_account_id}\n")
-    f.write(f"SNOWFLAKE_PASSWORD={snowflake_pass}\n")
-    f.write(f"SNOWFLAKE_USERNAME={snowflake_user}\n")
-    f.write(f"AWS_ACCOUNT_ID={aws_account_id}\n")
+def main():
+    snowflake_user, snowflake_pass, snowflake_account_id, aws_account_id = get_user_input()
+    write_to_file(".env", snowflake_user, snowflake_pass, snowflake_account_id, aws_account_id)
+    write_to_file(".transformation/dbt/.env", snowflake_user, snowflake_pass, snowflake_account_id, aws_account_id)
 
-with open(".transformation/dbt/.env", "w") as f:
-    f.write(f"SNOWFLAKE_ACCOUNT={snowflake_account_id}\n")
-    f.write(f"SNOWFLAKE_PASSWORD={snowflake_pass}\n")
-    f.write(f"SNOWFLAKE_USERNAME={snowflake_user}\n")
-    f.write(f"SNOWFLAKE_ROLE={snowflake_user}\n")
+if __name__ == "__main__":
+    main()
